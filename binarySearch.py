6f7a0de1b3ff77997c0930493b266ff685fe5f31
artemon-line <tyran77757@gmail.com>
2019-08-01 22:17:17 +0300

    add helper

--- binarySearch.py
+++ binarySearch.py
@@ -1,22 +1,29 @@
+import helper
+
+
 def binary_search(list, item):
     low = 0
-    high = len(list)-1
+    high = len(list) - 1
+    steps = 0
 
     while low <= high:
-        mid = (low + high)
+        mid = (low + high) / 2
         guess = list[mid]
+        steps += 1
         if guess == item:
-            return mid
+            helper.print_stat(len(list), steps)
+            return guess
         if guess > item:
             high = mid - 1
         else:
             low = mid + 1
+    helper.print_stat(len(list), steps)
     return None
 
 
 if __name__ == "__main__":
-    my_list = [1, 3, 5, 7, 9]
-    res1 = binary_search(my_list, 3)
+    my_list = [1, 3, 5, 7, 9, 11, 16, 17, 19, 22, 25, 27, 29, 33]
+    res1 = binary_search(my_list, 7)
     print(res1)  # => 1
     res2 = binary_search(my_list, -1)
     print(res2)  # => None
---
+++ helper.py
@@ -0,0 +1,2 @@
+def print_stat(arrLength, steps):
+    print("Alghorytm of {0} n toke {1} operation".format(arrLength, steps))
